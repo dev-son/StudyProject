fdf6d4144323af72e32735a410748a9105aa2508
Ik <jam44088@gmail.com>
2023-11-28 20:00:41 +0900

    feat: Unreal Project add

---
+++ Source/StudyProject/Public/SUnrealObjectClass.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SUnrealObjectClass.generated.h"
+
+/**
+ * 
+ */
+ UCLASS()
+class STUDYPROJECT_API SUnrealObjectClass : public UObject
+{
+	GENRATED_BODY()
+};
